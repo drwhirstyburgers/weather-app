0dcf02c4bae0b28c76a999921cec4d73406367bb
Drwhirstyburgers <dave.hirst50@gmail.com>
2018-12-06 15:07:30 -0500

    failure

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ const Api_Key = 'bfaa282bbd90de0a28b0d9774b321684';
 class App extends Component {
 
   state = {
-
     temperature: undefined,
     city: undefined,
     country: undefined,
@@ -17,18 +16,40 @@ class App extends Component {
     description: undefined,
     error: undefined
   }
+  componentWillMount(function() {
+  getCoords() {
+   if(navigator.geolocation) {
+     navigator.geolocation.getCurrentPosition(function(position) {
+       const latitude = position.coords.latitude;
+       const longitude = position.coords.longitude;
+     });
+    const api = fetch('api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}');
+    const resp = api.json();
+    console.log(resp);
+    this.setState({
+      temperature: resp.main.temp,
+      city: resp.name,
+      country: resp.sys.country,
+      humidity: resp.main.humidity,
+      description: resp.weather[0].description,
+      error: ""
+    });
+   }
+  }
+})
+
 
   getWeather = async(e) => {
     e.preventDefault();
-    const city = e.target.elements.city.value;
-    const country = e.target.elements.country.value;
+    let city = e.target.elements.city.value;
+    let country = e.target.elements.country.value;
     const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${Api_Key}`);
     const response = await api_call.json();
     console.log(response);
     if(response.message == 'city not found'){
       this.setState({
         error: "City not found, please try again",
-      })
+      });
     }else if(city && country){
       this.setState({
         temperature: response.main.temp,
@@ -37,11 +58,11 @@ class App extends Component {
         humidity: response.main.humidity,
         description: response.weather[0].description,
         error: ""
-      })
+      });
     }else{
       this.setState({
         error: "Please enter a city AND country"
-      })
+      });
     }
   }
 
